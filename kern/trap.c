4d22fda9cf0ea055c7e40ca86d79ec14df9f2980
BlackDragonF <obsidiandragon2016@gmail.com>
2018-02-27 09:11:02 +0800

    finish lab4 - exercise 4

--- kern/trap.c
+++ kern/trap.c
@@ -145,24 +145,47 @@ trap_init_percpu(void)
 	// user space on that CPU.
 	//
 	// LAB 4: Your code here:
+    // trap_init_percpu() is called by all CPUs
+    // set esp0 and ss0 of task state segment to provide
+    // per-CPU's kernel stack access
+    thiscpu->cpu_ts.ts_esp0 = (uintptr_t)percpu_kstacks[cpunum()];
+    thiscpu->cpu_ts.ts_ss0 = GD_KD;
+    // set IO map base address to prevent unauthorized environments
+    // this line works together as we'll set TSS segment limit later
+    // so all ports in address space have no corresponding IOPB
+    thiscpu->cpu_ts.ts_iomb = sizeof(struct Taskstate);
 
-	// Setup a TSS so that we get the right stack
-	// when we trap to the kernel.
-	ts.ts_esp0 = KSTACKTOP;
-	ts.ts_ss0 = GD_KD;
-	ts.ts_iomb = sizeof(struct Taskstate);
+    // set TSS in gdt
+    // 0 means RPL and sd_s = 0 means system segment
+    gdt[(GD_TSS0 >> 3) + cpunum()] = SEG16(STS_T32A, (uint32_t) (&(thiscpu->cpu_ts)),
+        sizeof(struct Taskstate) - 1, 0);
+    gdt[(GD_TSS0 >> 3) + cpunum()].sd_s = 0;
 
-	// Initialize the TSS slot of the gdt.
-	gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
-					sizeof(struct Taskstate) - 1, 0);
-	gdt[GD_TSS0 >> 3].sd_s = 0;
+    // Load TSS selector
+    ltr(GD_TSS0 + (cpunum() << 3));
 
-	// Load the TSS selector (like other segment selectors, the
-	// bottom three bits are special; we leave them 0)
-	ltr(GD_TSS0);
+    // load the IDT
+    lidt(&idt_pd);
 
-	// Load the IDT
-	lidt(&idt_pd);
+    /* old Lab3 code:
+	 * // Setup a TSS so that we get the right stack
+	 * // when we trap to the kernel.
+	 * ts.ts_esp0 = KSTACKTOP;
+	 * ts.ts_ss0 = GD_KD;
+	 * ts.ts_iomb = sizeof(struct Taskstate);
+     *
+	 * // Initialize the TSS slot of the gdt.
+	 * gdt[GD_TSS0 >> 3] = SEG16(STS_T32A, (uint32_t) (&ts),
+	 * 				sizeof(struct Taskstate) - 1, 0);
+	 * gdt[GD_TSS0 >> 3].sd_s = 0;
+     *
+	 * // Load the TSS selector (like other segment selectors, the
+	 * // bottom three bits are special; we leave them 0)
+	 * ltr(GD_TSS0);
+     *
+	 * // Load the IDT
+	 * lidt(&idt_pd);
+     */
 }
 
 void
